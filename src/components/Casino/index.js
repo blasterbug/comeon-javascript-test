e2fe9e4bdca9e72683ad72c87e9bedd7b819867a
blasterbug <tontonposso@gmail.com>
2018-02-06 12:40:51 +0100

    filter games (dumb search)
    by category or by search

--- src/components/Casino/index.js
+++ src/components/Casino/index.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import propTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { browserHistory } from 'react-router';
+import _ from 'lodash';
 
 import {
   listCategories,
@@ -22,23 +23,50 @@ class Casino extends Component {
     this.state = {
       searchQuery: '',
       games: props.games,
-      category: 0
+      category: 0 // ALL
     };
     this.logout = this.logout.bind( this );
     this.searchGame = this.searchGame.bind( this );
-  }
-
-  searchGame( event ) {
-    this.setState({ [event.target.name]: event.target.value });
+    this.selectCategory = this.selectCategory.bind( this );
   }
 
   logout( event ) {
     logoutUser( this.props.user.username );
   }
 
+  searchGame( event ) {
+    const searchQuery = event.target.value;
+    this.setState({
+      searchQuery,
+      // use this.state.games to filtered displayed games. i.e. filtered by categories
+      games: _.filter( this.props.games, game => {
+        return (
+          game.name.toLowerCase().indexOf( searchQuery.toLowerCase() ) >= 0
+        || game.description.toLowerCase().indexOf( searchQuery.toLowerCase() ) >= 0
+        );
+      })
+    } );
+  }
+
+  selectCategory( categoryId ) {
+    this.setState({
+      category: categoryId,
+      games: _.filter( this.props.games, game => {
+        return _.includes( game.categoryIds, categoryId )
+      } )
+    });
+  }
+
+
   componentDidMount() {
     listCategories();
     listGames();
+  }
+
+  componentWillReceiveProps( nextProps ) {
+    if ( !_.isEqual( nextProps.games, this.state.games ) ) {
+      this.setState({ games: nextProps.games });
+    }
   }
 
   componentWillUpdate( nextProps, nextState ) {
@@ -50,46 +78,51 @@ class Casino extends Component {
   render() {
     const {
       categories,
-      games,
       user
     } = this.props;
+    const { games } = this.state;
     return (
-      <div className="casino">
-        <div className="ui grid centered">
-          <div className="twelve wide column">
-            <div className="ui list">
-              <div className="player item">
+      <div className="casino" >
+        <div className="ui grid centered" >
+          <div className="twelve wide column" >
+            <div className="ui list" >
+              <div className="player item" >
                 <img className="ui avatar image" src={ user.avatar } alt="avatar" />
-                <div className="content">
-                  <div className="header">
-                    <b className="name">
+                <div className="content" >
+                  <div className="header" >
+                    <b className="name" >
                       { user.name }
                     </b>
                   </div>
-                  <div className="description event">
+                  <div className="description event" >
                     { user.event }
                   </div>
                 </div>
               </div>
             </div>
-            <div onClick={ this.logout } className="logout ui left floated secondary button inverted">
-              <i className="left chevron icon"></i>
+            <div onClick={ this.logout } className="logout ui left floated secondary button inverted" >
+              <i className="left chevron icon" ></i>
               Log Out
               </div>
             </div>
-            <div className="four wide column">
-              <div className="search ui small icon input ">
-                <input onChange={ this.searchGame } type="text" placeholder="Search Game" />
-                <i className="search icon"></i>
+            <div className="four wide column" >
+              <div className="search ui small icon input" >
+                <input onChange={ this.searchGame } name="searchQuery" type="text" placeholder="Search Game" />
+                <i className="search icon" ></i>
               </div>
             </div>
           </div>
-          <div className="ui grid">
-            <div className="twelve wide column">
-              <h3 className="ui dividing header">Games</h3>
-              <div className="ui relaxed divided game items links">
+          <div className="ui grid" >
+            <div className="twelve wide column" >
+              <h3 className="ui dividing header" >Games</h3>
+              <div className="ui relaxed divided game items links" >
                 {
-                  games.map( game => {
+                  // display loader while fetching
+                  this.props.isFetching ?
+                    <div className="ui active inverted dimmer">
+                      <div className="ui text loader">Loading</div>
+                    </div>
+                  : games.map( game => {
                     return <Game
                       key={ game.code }
                       categoryIds={ game.categoryIds }
@@ -101,16 +134,18 @@ class Casino extends Component {
                 }
               </div>
             </div>
-            <div className="four wide column">
-              <h3 className="ui dividing header">Categories</h3>
-              <div className="ui selection animated list category items">
-                <div className="content">
+            <div className="four wide column" >
+              <h3 className="ui dividing header" >Categories</h3>
+              <div className="ui selection animated list category items" >
                 {
                   categories.map( category => {
-                    return <Category key={ category.id } id={ category.id } name={ category.name } />
+                    return <Category
+                      key={ category.id }
+                      id={ category.id }
+                      name={ category.name }
+                      onClick={ this.selectCategory } />
                   } )
                 }
-              </div>
               </div>
             </div>
           </div>
@@ -137,6 +172,7 @@ Casino.propTypes = {
     })
   ).isRequired,
   isAuthenticated: propTypes.bool.isRequired,
+  isFetching: propTypes.bool.isRequired,
   user: propTypes.shape({
     avatar: propTypes.string,
     event: propTypes.string,
@@ -149,8 +185,8 @@ Casino.defaultProps = {
   categories: [],
   errorMessage: '',
   games : [],
-  infoMessage: '',
   isAuthenticated: false,
+  isFetching: false,
   user: {},
 };
 
@@ -160,6 +196,7 @@ function mapStateToProps( state ) {
     categories: state.casino.categories,
     games: state.casino.games,
     isAuthenticated: state.auth.isAuthenticated,
+    isFetching: state.casino.isFetching,
     user: state.auth.user
   };
 }
